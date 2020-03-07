22fbf7161a8f949f30f38356244ec4e7c2dfd6fb
Muah <ctf0@users.noreply.github.com>
2020-03-07 07:09:35 +0200

    Update tags.php

--- src/config/tags.php
+++ src/config/tags.php
@@ -4,7 +4,7 @@ return [
     /*
      * The given function generates a URL friendly "slug" from the tag name property before saving it.
      */
-    'slugger' => 'str_slug',
+    'slugger' => null,
 
     /*
      * package controller
